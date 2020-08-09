1f67d46862d910beda40946a5c1b8e3f8f269dad
Jac Diamond <jac.diamond@hotmail.com>
2020-08-09 11:49:55 +1000

    Split up files

--- hashMasher.py
+++ hashMasher.py
@@ -1,10 +1,6 @@
 import os
 import time
 import argparse
-import requests
-import fabulous
-import hashlib
-from colorama import Fore, Back, Style
 
 vtAPIKey = os.environ.get('VTAPIKey')
 virusTotalBase = "https://www.virustotal.com/vtapi/v2/"
@@ -20,71 +16,6 @@ banner = """
 														   
 """
 
-def printOutput(vtOutput, name, number):
-	if vtOutput['response_code'] == 1:
-		detectionPercentage = vtOutput['positives']/vtOutput['total']
-
-	if vtOutput['response_code'] == 0:
-		print(Back.WHITE + Fore.BLACK)
-		print(str(number) + ": " + name + Style.RESET_ALL)
-		print("Resource: " + vtOutput['resource'])
-		print("No VirusTotal results found")
-		return
-
-	if detectionPercentage == 0:
-		print(Back.GREEN)
-	elif detectionPercentage < 0.1:
-		print('\033[43m')
-	else:
-		print(Back.RED)
-	print(str(number) + ": " + name + " - " + str(vtOutput['positives']) + "/" + str(vtOutput['total']) + Style.RESET_ALL)
-	print("MD5: " + vtOutput['md5'])
-	print("SHA1: " + vtOutput['sha1'])
-	print("SHA256: " + vtOutput['sha256'] + "\n")
-	print("VirusTotal Link: " + vtOutput['permalink'])
-
-
-def queryVirusTotal(hash):
-	params = {
-	'apikey': vtAPIKey,
-	'resource': hash
-	}
-	url = virusTotalBase + vtFileReport 
-	try:
-		r = session.get(virusTotalBase + vtFileReport, params=params)
-		return r.json()
-
-	except requests.exceptions.RequestException as e:
-		raise SystemExit(e)
-		return 0
-
-
-def getHashList(path):
-	hashes = []
-	files = []
-	#Recurse through all files in the directory
-	if args.recursive:
-		for dirpath, dirnames, files in os.walk(path):
-			for file in files:
-				hashes.append(getFileHash(file))
-				files.append(file)
-	#Not recursive - just go through files in that directory
-	else:
-		for file in os.listdir(path):
-			if os.path.isfile(os.path.join(path,file)):
-				hashes.append(getFileHash(os.path.join(path,file)))
-				files.append(file)
-	return hashes, files
-
-
-def getFileHash(file):
-	newHash = hashlib.sha256()
-	with open(file, 'rb') as f:
-		fileBlock = f.read(fileBlockSize)
-		while len(fileBlock) > 0:
-			newHash.update(fileBlock)
-			fileBlock = f.read(fileBlockSize)
-	return newHash.hexdigest()
 
 
 def main():
@@ -96,6 +27,7 @@ def main():
 		for i in range(0, len(hashes)):
 			printOutput(queryVirusTotal(hashes[i]), files[i], i + 1)
 			time.sleep(15)
+
 
 if __name__ == "__main__":
 	print(banner)
